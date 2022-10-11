2e89b15c67a506c6fea0b5ddc1ece2fe49e5b08a
Omar Faruk <farukk6319@gmail.com>
2022-10-11 16:16:01 +0600

    Quiz alert added

--- src/components/QuizOptions/QuizOptions.js
+++ src/components/QuizOptions/QuizOptions.js
@@ -1,19 +1,17 @@
 import React from 'react';
 import './QuizOptions.css';
-import { EyeIcon } from "@heroicons/react/24/solid";
-
+import { EyeIcon} from "@heroicons/react/24/solid";
 
 const QuizOptions = ({ quiz }) => {
   const {options, question} = quiz;
-  const answer = quiz.correctAnswer
+  const quizAnswer = quiz.correctAnswer
   const handleQuiz = (event) => {
-    const question = event.target.innerText;
-    console.log(question)
-    if (question === answer) {
-       alert('correct')
-    }
-    else {
-      alert('wrong')
+    const quizQuestion = event.target.innerText;
+    console.log(quizQuestion);
+    if (quizQuestion === quizAnswer) {
+      alert('correct answer')
+    } else {
+      alert("wrong");
     }
   } 
   const handleWithIcon = () => {
@@ -23,12 +21,15 @@ const QuizOptions = ({ quiz }) => {
   return (
     <div>
       <h1 className="bg-blue-300">{question}</h1>
-      <h1>{answer}</h1>
-      <div className="quiz-list">
-        <EyeIcon onClick={handleWithIcon} className="h-6 w-6 text-blue-500" />
-        {options.map((option) => (
-          <h1 onClick={(event) => handleQuiz(event)}>{option}</h1>
-        ))}
+
+      <h1>{quizAnswer}</h1>
+      <div>
+        <EyeIcon onClick={handleWithIcon} className="h-6 w-6 ml-96"/>
+        <div onClick={(event) => handleQuiz(event)} className="quiz-list">
+          {options.map((option) => (
+            <h1>{option}</h1>
+          ))}
+        </div>
       </div>
     </div>
   );
